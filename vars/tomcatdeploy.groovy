01985163ffe2e23c4157a4ffad2286506e056f1f
SamiyaSulthana <128040522+SamiyaSulthana@users.noreply.github.com>
2023-04-05 17:32:30 +0530

    Create tomcatdeploy.groovy

---
+++ vars/tomcatdeploy.groovy
@@ -0,0 +1,6 @@
+def call(user,ip){
+sshagent(['TomcatDev']) {
+                sh "scp -o StrictHostKeyChecking=no target/hr-api.war ${user}@${ip}:/opt/sammu/webapps/"
+                    sh "ssh ${user}@${ip} /opt/sammu/bin/shutdown.sh"
+                    sh "ssh ${user}@${ip} /opt/sammu/bin/startup.sh"
+}
